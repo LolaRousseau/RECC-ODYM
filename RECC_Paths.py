2e4cde66681fc300029be07da6401b72d75fb670
Lola <lola.s.a.rousseau@ntnu.no>
2024-07-08 12:23:37 +0200

    Adding RECC_Paths file

---
+++ RECC_Paths.py
@@ -0,0 +1,7 @@
+import os 
+
+current_path = os.path.abspath(os.getcwd())
+
+odym_path         = os.path.join(current_path,'odym')
+data_path         = os.path.join(current_path,'CURRENT_VN1_0')
+results_path      = os.path.join(current_path,'results')
